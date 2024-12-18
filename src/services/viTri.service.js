cc8deb8844b5c48d01ba874ad0dcbc8280ff8d1e
KhaiTrn <khai.tran.srp@gmail.com>
2024-12-19 00:09:15 +0700

    merge managerRoom and start managerLocation

--- src/services/viTri.service.js
+++ src/services/viTri.service.js
@@ -3,4 +3,12 @@ export const locationService = {
   getViTri: () => {
     return http.get("/vi-tri");
   },
+  getAllLocation: (pageIndex, keyword) => {
+    return http.get(
+      `/phan-trang-tim-kiem?pageIndex=${pageIndex}&pageSize=10&keyword=${keyword}`
+    );
+  },
+  postLocation: () => {
+    return http.post("/vi-tri");
+  },
 };
